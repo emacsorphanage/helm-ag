2ea8ca5d4a63b18319b796e378f9b236cd715869
Syohei YOSHIDA <syohex@gmail.com>
2013-03-01 16:33:53 +0900

    Add dependencies

--- helm-ag.el
+++ helm-ag.el
@@ -5,6 +5,7 @@
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-ag
 ;; Version: 0.01
+;; Package-Requires: ((helm "1.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
