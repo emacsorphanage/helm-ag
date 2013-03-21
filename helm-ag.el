f3a2dbe6c2d4e11d618ddaac0571e8a88b0320f8
Syohei YOSHIDA <syohex@gmail.com>
2013-03-21 22:19:08 +0900

    Fix case that thing-at-point returns nil

--- helm-ag.el
+++ helm-ag.el
@@ -67,11 +67,12 @@
            (if helm-ag-command-option
                (format " %s" helm-ag-command-option)
              "")
-           (if helm-ag-insert-at-point
-               (format " %s"
-                       (with-helm-current-buffer
-                         (thing-at-point helm-ag-insert-at-point)))
-             " "))))
+           (let ((at-point (thing-at-point helm-ag-insert-at-point)))
+             (if (and helm-ag-insert-at-point at-point)
+                 (format " %s"
+                         (with-helm-current-buffer
+                           (thing-at-point helm-ag-insert-at-point)))
+               " ")))))
 
 (defun helm-ag-init ()
   (let* ((cmd (read-string "Ag: "
