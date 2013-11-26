d8713d6b4251cad11b5d4de3244bad808d775a48
Syohei YOSHIDA <syohex@gmail.com>
2013-11-26 11:34:10 +0900

    Bump up version and update changes

--- helm-ag.el
+++ helm-ag.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-ag
-;; Version: 0.09
+;; Version: 0.10
 ;; Package-Requires: ((helm "1.5.6"))
 
 ;; This program is free software; you can redistribute it and/or modify
