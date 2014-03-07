bbb139c3499fb2bc4e01abe757b98e4379837dd8
Syohei YOSHIDA <syohex@gmail.com>
2014-03-08 08:57:08 +0900

    Update requirement cl-lib version

--- helm-ag.el
+++ helm-ag.el
@@ -5,7 +5,7 @@
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-ag
 ;; Version: 0.14
-;; Package-Requires: ((helm "1.5.6") (cl-lib "0.4"))
+;; Package-Requires: ((helm "1.5.6") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
