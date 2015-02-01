91d8034e390dc53bd31c5302b4a2692ef4da0f54
Syohei YOSHIDA <syohex@gmail.com>
2015-02-01 23:18:48 +0900

    Bump up version and update Changes

--- helm-ag.el
+++ helm-ag.el
@@ -1,10 +1,10 @@
 ;;; helm-ag.el --- the silver searcher with helm interface -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014 by Syohei YOSHIDA
+;; Copyright (C) 2015 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-ag
-;; Version: 0.32
+;; Version: 0.33
 ;; Package-Requires: ((helm "1.5.6") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
