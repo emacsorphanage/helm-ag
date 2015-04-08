d385f3773c999411f9ea3c71af85c0c8eabb5a8a
Syohei YOSHIDA <syohex@gmail.com>
2015-04-09 00:01:01 +0900

    Add flag for using Emacs lisp regexp instead of PCRE

--- helm-ag.el
+++ helm-ag.el
@@ -89,6 +89,11 @@ They are specified to `--ignore' options."
   :type 'boolean
   :group 'helm-ag)
 
+(defcustom helm-ag-use-emacs-lisp-regexp nil
+  "[Experimental] Use Emacs Lisp regexp instead of PCRE."
+  :type 'boolean
+  :group 'helm-ag)
+
 (defvar helm-ag--command-history '())
 (defvar helm-ag--context-stack nil)
 (defvar helm-ag--default-directory nil)
@@ -344,6 +349,8 @@ They are specified to `--ignore' options."
          (query (read-string "Pattern: " searched-word 'helm-ag--command-history)))
     (when (string= query "")
       (error "Input is empty!!"))
+    (when helm-ag-use-emacs-lisp-regexp
+      (setq query (helm-ag--elisp-regexp-to-pcre query)))
     (setq helm-ag--last-query query
           helm-ag--elisp-regexp-query (helm-ag--pcre-to-elisp-regexp query))
     (setq helm-ag--valid-regexp-for-emacs
