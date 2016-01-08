670d20c46a2f6d4a001ec40e597d52f6956fa387
Syohei YOSHIDA <syohex@gmail.com>
2016-01-08 17:56:18 +0900

    Update copyright year

--- test/test-util.el
+++ test/test-util.el
@@ -1,6 +1,6 @@
 ;;; test-util.el --- test helm-ag utility functions
 
-;; Copyright (C) 2014 by Syohei YOSHIDA
+;; Copyright (C) 2016 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 
