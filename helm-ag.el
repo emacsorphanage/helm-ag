6fdad5e4f4888bf94e841b7e2f67cc9d6b4d374b
Kien Nguyen <kien.n.quang@gmail.com>
2020-03-28 12:14:28 +0900

    travis: remove travis badge and update env to support from emacs-25

--- helm-ag.el
+++ helm-ag.el
@@ -5,7 +5,7 @@
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-ag
 ;; Version: 0.59
-;; Package-Requires: ((emacs "24.4") (helm "2.0"))
+;; Package-Requires: ((emacs "25.1") (helm "2.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
