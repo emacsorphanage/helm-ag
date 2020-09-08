20b5659279b39d74620aa88cdc3c80a2b5479ace
JenChieh <jcs090218@gmail.com>
2020-09-08 16:21:05 +0800

    Revert changes.

--- helm-ag.el
+++ helm-ag.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-ag
-;; Version: 0.63
+;; Version: 0.62
 ;; Package-Requires: ((emacs "25.1") (helm "2.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
